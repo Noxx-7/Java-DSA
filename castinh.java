b3c2fdbc5dba59344f50789c2530323dbaebc487
Rahul sharma <83027100+Noxx-7@users.noreply.github.com>
2022-06-19 12:57:37 +0530

    Add files via upload

---
+++ castinh.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class castinh {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+// typecasting
+//        int num = (int)(16.555);
+//        System.out.println(num);
+
+        // automatic type promotion in expression
+
+         byte a = 40;
+         byte b = 50;
+         byte c = 100;
+
+         int d =  a*b/c;
+        System.out.println(d);
+
+    }
+}
+
