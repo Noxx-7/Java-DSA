aa565c6616798f061f589c839433dd175219425a
Rahul sharma <83027100+Noxx-7@users.noreply.github.com>
2022-06-19 12:56:25 +0530

    Add files via upload

---
+++ primitive.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class primitive {
+    public static void main(String[] args) {
+//        int rollno = 45;
+//        char letter  = 'r';
+//        float marks = 96.25f;
+//        double largedecimalnumver = 5454.2225453;
+//        long largeint = 44444444444444L;
+//        boolean check = true;
+        Scanner input = new Scanner(System.in);
+        int roll = input.nextInt();
+        System.out.println("Your roll is " +roll);
+
+    }
+}
