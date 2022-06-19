d3a58536dc43f85f16489b367043a7e117ddbb39
Rahul sharma <83027100+Noxx-7@users.noreply.github.com>
2022-06-19 23:32:25 +0530

    Add files via upload

---
+++ loop.java
@@ -0,0 +1,50 @@
+//import java.util.Scanner;
+//
+//public class loop {
+//    public static void main(String[] args) {
+////
+////        while loop
+////        int count = 1;
+////        while(count !=9){
+////            System.out.println(count);
+////            count ++;
+//
+//// for loop
+//
+//
+//        }
+//    }
+//
+
+// assiment 1
+//import java.util.Scanner;
+//
+//public class loop {
+//    public static void main(String[] args) {
+//        Scanner input = new Scanner(System.in);
+//        int a = input.nextInt();
+//        int b = input.nextInt();
+//
+//        int sum = a +b;
+//        System.out.println("Your number is " +sum);
+//    }
+//}
+
+//Take a number as input and print the multiplication table for it.
+//import java.util.Scanner;
+//
+//public class loop {
+//    public static void main(String[] args) {
+//        Scanner s = new Scanner(System.in);
+//        int n = s.nextInt();
+//        for (int i = 1; i<=10; i++){
+//            System.out.println(n+"*"+i+"="+n*i);
+//        }
+//    }
+//}
+//
+
+
+
+
+
