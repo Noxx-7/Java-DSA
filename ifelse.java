02c96c2e8edbdfdd3c81a1c1b662d5110ea11f9b
Rahul sharma <83027100+Noxx-7@users.noreply.github.com>
2022-06-21 12:48:52 +0530

    Add files via upload

---
+++ ifelse.java
@@ -0,0 +1,12 @@
+public class ifelse {
+    public static void main(String[] args) {
+        int salary = 2000;
+        if (salary >1000){
+            salary =salary+1000;
+
+        }else{
+            salary =salary+1500;
+        }
+        System.out.println(salary);
+    }
+}
